35f04e4f317b5f09f7311016c362cac66a9480fc
bobxiong88 <bobxiong88@gmail.com>
2022-06-04 16:39:50 -0700

    Created display

--- javascript.js
+++ javascript.js
@@ -10,3 +10,21 @@ function multiply(a, b){
 function divide(a, b){
     return a/b;
 }
+function pow(a, b){
+    return a**b;
+}
+function mod(a, b){
+    return a%b;
+}
+function mystery(a, b){
+    return (a+b)*(a-b);
+}
+function operate(a, b, operator){
+    if (operator==="+") return add(a,b);
+    else if (operator==="-") return subtract(a,b);
+    else if (operator==="*") return multiply(a,b);
+    else if (operator==="÷") return divide(a,b);
+    else if (operator==="^") return pow(a,b);
+    else if (operator==="%") return mod(a,b);
+    else return mystery(a,b);
+}
