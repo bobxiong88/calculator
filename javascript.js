14f7572c08c0b7add40f33a3b32f0c2a06673e83
bobxiong88 <bobxiong88@gmail.com>
2022-06-04 18:23:31 -0700

    Final commit

--- javascript.js
+++ javascript.js
@@ -20,6 +20,8 @@ function mystery(a, b){
     return (a+b)*(a-b);
 }
 function operate(a, b, operator){
+    a = parseFloat(a);
+    b = parseFloat(b);
     if (operator==="+") return add(a,b);
     else if (operator==="-") return subtract(a,b);
     else if (operator==="*") return multiply(a,b);
@@ -28,3 +30,77 @@ function operate(a, b, operator){
     else if (operator==="%") return mod(a,b);
     else return mystery(a,b);
 }
+
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector("#display");
+
+let displayText = "";
+let prevNumber = "";
+let currOp = "";
+let oped = false;
+let eqed = false;
+let error = false;
+
+let operators = "+-*÷^%m";
+let digits = "0123456789."
+
+let n = 20;
+
+buttons.forEach((button)=>{
+    button.addEventListener('click', ()=>{
+        let char = button.id;
+        if (char==="CE"||error === true){
+            displayText = "";
+            prevNumber = "";
+            oped = false;
+            eqed = false;
+            error = false;
+        }
+        if (digits.includes(char)) {
+            if (eqed ===true){
+                prevNumber = "";
+                displayText = "";
+                eqed = false;
+            }
+            else if (oped===true){
+                prevNumber = displayText;
+                displayText = "";
+                oped = false;
+            }
+            if ((char==="."&&!displayText.includes(char))||char!==".") displayText = displayText+char;  
+        }
+        else if (operators.includes(char)){
+            if (displayText!==""){
+                if (eqed === true){
+                    prevNumber = "";
+                }
+                if (prevNumber!==""){
+                    displayText = operate(prevNumber, displayText, currOp);
+                    prevNumber = "";
+                }
+                else{
+                    prevNumber = displayText;
+                }
+                currOp = char;
+                oped = true;
+                eqed = false;
+            }
+        }
+        else if (char==="="){
+            if (displayText!==""&&prevNumber!==""){
+                if (eqed === true){
+                    displayText = operate(displayText, prevNumber, currOp);
+                }
+                else{
+                    [prevNumber,displayText] = [displayText, operate(prevNumber,displayText,currOp)];
+                }
+                eqed = true;
+            }
+        }
+        if (displayText.length>20){
+            displayText = "ERROR";
+            error = true;
+        }
+        display.textContent = displayText;
+    });
+});
